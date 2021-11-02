aa0797da346e26a6255ec3613372256a82899275
Niklas Köhnecke <niklas.koehnecke@gmail.com>
2021-11-02 15:43:19 +0100

    More MVP predictor experimentation

--- wikipedia_cleanup/baseline.py
+++ wikipedia_cleanup/baseline.py
@@ -2,10 +2,11 @@ import numpy as np
 
 
 def next_change(previous_change_timestamps):
-    previous_change_timestamps = np.sort(np.unique(previous_change_timestamps))
+    # np unique sorts
+    previous_change_timestamps = np.sort(previous_change_timestamps)
     if len(previous_change_timestamps) < 2:
         return None
-    mean_time_to_change = np.median(
+    mean_time_to_change = np.mean(
         previous_change_timestamps[1:] - previous_change_timestamps[0:-1]
     )
     return previous_change_timestamps[-1] + mean_time_to_change
