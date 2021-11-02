b57634377dda0956bab2cedd1aa4ee67660e9329
Niklas Köhnecke <niklas.koehnecke@gmail.com>
2021-11-02 13:21:01 +0100

    Fix pre-commit errors

--- wikipedia_cleanup/data_processing.py
+++ wikipedia_cleanup/data_processing.py
@@ -1,7 +1,7 @@
 import itertools
 import json
 from pathlib import Path
-from typing import Optional, Iterable, Tuple, Any
+from typing import Any, Iterable, Optional, Tuple
 
 import pandas as pd
 from tqdm import tqdm
@@ -16,26 +16,26 @@ def read_file(file_path: Path) -> Iterable[Tuple[Any, ...]]:
         for line in f:
             revision = InfoboxRevision(**json.loads(line))
             for change in revision.changes:
-                changes.append((
-                    revision.key,
-                    revision.revisionId,
-                    revision.pageID,
-                    revision.pageTitle,
-                    change.property.name,
-                    change.previousValue,
-                    change.currentValue,
-                    revision.validFrom,
-                    change.valueValidTo
-                ))
+                changes.append(
+                    (
+                        revision.key,
+                        revision.revisionId,
+                        revision.pageID,
+                        revision.pageTitle,
+                        change.property.name,
+                        change.previousValue,
+                        change.currentValue,
+                        revision.validFrom,
+                        change.valueValidTo,
+                    )
+                )
     return changes
 
 
-def get_data(input_path: Path, n_files: Optional[int] = None, n_jobs: int = 0) -> pd.DataFrame:
-    files = [
-        x
-        for x in Path(input_path).rglob("*.output.json")
-        if x.is_file()
-    ]
+def get_data(
+    input_path: Path, n_files: Optional[int] = None, n_jobs: int = 0
+) -> pd.DataFrame:
+    files = [x for x in Path(input_path).rglob("*.output.json") if x.is_file()]
     if n_jobs > n_files or n_jobs > len(files):
         n_jobs = min(n_files, len(files))
     files = files[slice(n_files)]
@@ -57,6 +57,6 @@ def get_data(input_path: Path, n_files: Optional[int] = None, n_jobs: int = 0) -
             "previousValue",
             "currentValue",
             "validFrom",
-            "validTo"
+            "validTo",
         ],
     )
--- wikipedia_cleanup/simple_archive_read.py
+++ wikipedia_cleanup/simple_archive_read.py
@@ -1,15 +1,15 @@
 import json
 import pickle
+from datetime import datetime
 from pathlib import Path
-import pandas as pd
-import numpy as np
 
 import Levenshtein
 import libarchive.public
+import numpy as np
+import pandas as pd
 from tqdm.contrib.concurrent import process_map
-from datetime import datetime
 
-raw_data_folder = Path('/home/secret/uni/Masterprojekt/data/matched-infoboxes-raw')
+raw_data_folder = Path("/home/secret/uni/Masterprojekt/data/matched-infoboxes-raw")
 
 
 def analyse_string_numeric(archive_path):
@@ -19,26 +19,26 @@ def analyse_string_numeric(archive_path):
     string_to_numeric = 0
     with libarchive.public.file_reader(str(archive_path)) as archive:
         for entry in archive:
-            content_bytes = bytearray('', encoding='utf_8')
+            content_bytes = bytearray("", encoding="utf_8")
             for block in entry.get_blocks():
                 content_bytes += block
-            content = content_bytes.decode(encoding='utf_8')
-            jsonObjs = content.split('\n')
+            content = content_bytes.decode(encoding="utf_8")
+            jsonObjs = content.split("\n")
             for jsonObj in filter(lambda x: x, jsonObjs):
                 obj = json.loads(jsonObj)
-                changes = obj['changes']
+                changes = obj["changes"]
                 for change in changes:
                     curr_val_number = None
                     prev_val_number = None
-                    if 'previousValue' in change:
+                    if "previousValue" in change:
                         try:
-                            float(change['previousValue'])
+                            float(change["previousValue"])
                             prev_val_number = True
                         except ValueError:
                             prev_val_number = False
-                    if 'currentValue' in change:
+                    if "currentValue" in change:
                         try:
-                            float(change['currentValue'])
+                            float(change["currentValue"])
                             curr_val_number = True
                         except ValueError:
                             curr_val_number = False
@@ -56,39 +56,50 @@ def analyse_string_numeric(archive_path):
 def assert_every_change_changes_the_value(archive_path):
     with libarchive.public.file_reader(str(archive_path)) as archive:
         for entry in archive:
-            content_bytes = bytearray('', encoding='utf_8')
+            content_bytes = bytearray("", encoding="utf_8")
             for block in entry.get_blocks():
                 content_bytes += block
-            content = content_bytes.decode(encoding='utf_8')
-            jsonObjs = content.split('\n')
+            content = content_bytes.decode(encoding="utf_8")
+            jsonObjs = content.split("\n")
             for jsonObj in filter(lambda x: x, jsonObjs):
                 obj = json.loads(jsonObj)
-                changes = obj['changes']
+                changes = obj["changes"]
                 for change in changes:
-                    if 'previousValue' in change.keys() and 'currentValue' in change.keys():
-                        assert (change['previousValue'] != change['currentValue'])
+                    if (
+                        "previousValue" in change.keys()
+                        and "currentValue" in change.keys()
+                    ):
+                        assert change["previousValue"] != change["currentValue"]
 
 
 def get_every_change_size(archive_path):
     change_sizes = []
     with libarchive.public.file_reader(str(archive_path)) as archive:
         for entry in archive:
-            content_bytes = bytearray('', encoding='utf_8')
+            content_bytes = bytearray("", encoding="utf_8")
             for block in entry.get_blocks():
                 content_bytes += block
-            content = content_bytes.decode(encoding='utf_8')
-            jsonObjs = content.split('\n')
+            content = content_bytes.decode(encoding="utf_8")
+            jsonObjs = content.split("\n")
             for jsonObj in filter(lambda x: x, jsonObjs):
                 obj = json.loads(jsonObj)
-                changes = obj['changes']
+                changes = obj["changes"]
                 for change in changes:
-                    if 'previousValue' in change.keys() and 'currentValue' in change.keys():
-                        assert (change['previousValue'] != change['currentValue'])
-                        change_sizes.append(Levenshtein.distance(change['previousValue'], change['currentValue']))
+                    if (
+                        "previousValue" in change.keys()
+                        and "currentValue" in change.keys()
+                    ):
+                        assert change["previousValue"] != change["currentValue"]
+                        change_sizes.append(
+                            Levenshtein.distance(
+                                change["previousValue"], change["currentValue"]
+                            )
+                        )
     return change_sizes
 
 
 # TODO test bot reverts on local dataset.
+
 
 def count_creation_and_deletion_numbers(archive_path):
     creations = 0
@@ -96,19 +107,25 @@ def count_creation_and_deletion_numbers(archive_path):
     edits = 0
     with libarchive.public.file_reader(str(archive_path)) as archive:
         for entry in archive:
-            content_bytes = bytearray('', encoding='utf_8')
+            content_bytes = bytearray("", encoding="utf_8")
             for block in entry.get_blocks():
                 content_bytes += block
-            content = content_bytes.decode(encoding='utf_8')
-            jsonObjs = content.split('\n')
+            content = content_bytes.decode(encoding="utf_8")
+            jsonObjs = content.split("\n")
             for jsonObj in filter(lambda x: x, jsonObjs):
                 obj = json.loads(jsonObj)
-                changes = obj['changes']
+                changes = obj["changes"]
                 for change in changes:
                     edits += 1
-                    if 'previousValue' in change.keys() and 'currentValue' not in change.keys():
+                    if (
+                        "previousValue" in change.keys()
+                        and "currentValue" not in change.keys()
+                    ):
                         deletions += 1
-                    if 'previousValue' not in change.keys() and 'currentValue' in change.keys():
+                    if (
+                        "previousValue" not in change.keys()
+                        and "currentValue" in change.keys()
+                    ):
                         creations += 1
     return creations, deletions, edits
 
@@ -117,26 +134,31 @@ def get_all_valid_times(archive_path):
     valid_times = []
     with libarchive.public.file_reader(str(archive_path)) as archive:
         for entry in archive:
-            content_bytes = bytearray('', encoding='utf_8')
+            content_bytes = bytearray("", encoding="utf_8")
             for block in entry.get_blocks():
                 content_bytes += block
-            content = content_bytes.decode(encoding='utf_8')
-            jsonObjs = content.split('\n')
+            content = content_bytes.decode(encoding="utf_8")
+            jsonObjs = content.split("\n")
             for jsonObj in filter(lambda x: x, jsonObjs):
                 obj = json.loads(jsonObj)
-                changes = obj['changes']
-                valid_from = obj['validFrom']
+                changes = obj["changes"]
+                valid_from = obj["validFrom"]
                 for change in changes:
-                    if 'valueValidTo' in change.keys():
-                        delta = (datetime.strptime(change['valueValidTo'], '%Y-%m-%dT%H:%M:%SZ') - datetime.strptime(valid_from, '%Y-%m-%dT%H:%M:%SZ')).total_seconds()
+                    if "valueValidTo" in change.keys():
+                        delta = (
+                            datetime.strptime(
+                                change["valueValidTo"], "%Y-%m-%dT%H:%M:%SZ"
+                            )
+                            - datetime.strptime(valid_from, "%Y-%m-%dT%H:%M:%SZ")
+                        ).total_seconds()
                         valid_times.append(delta)
     return valid_times
 
 
-if __name__ == '__main__':
-    input_files = list(raw_data_folder.rglob('*.7z'))
+if __name__ == "__main__":
+    input_files = list(raw_data_folder.rglob("*.7z"))
 
-    '''res = process_map(analyse_string_numeric, input_files, max_workers=3)
+    """res = process_map(analyse_string_numeric, input_files, max_workers=3)
     numeric = 0
     string = 0
     numeric_to_string = 0
@@ -152,9 +174,9 @@ if __name__ == '__main__':
     print(f'string: {string} \t\t % {string / (numeric + string)}')
     print('\n\nType Changes\n\n')
     print(f'numeric to string: {numeric_to_string} \t\t % {numeric_to_string / (numeric + string)}')
-    print(f'string to numeric: {string_to_numeric} \t\t % {string_to_numeric / (numeric + string)}')'''
+    print(f'string to numeric: {string_to_numeric} \t\t % {string_to_numeric / (numeric + string)}')"""
 
-    '''numeric: 4749733 		 % 0.0811687487193842
+    """numeric: 4749733 		 % 0.0811687487193842
         string: 53767037 		 % 0.9188312512806158
         
         
@@ -162,12 +184,12 @@ if __name__ == '__main__':
         
         
         numeric to string: 622079 		 % 0.01063078156911258
-        string to numeric: 849259 		 % 0.014513087444847007'''
+        string to numeric: 849259 		 % 0.014513087444847007"""
 
     # process_map(assert_every_change_changes_the_value, input_files, max_workers=4)
     # asserts match
 
-    '''res = process_map(get_every_change_size, input_files, max_workers=3)
+    """res = process_map(get_every_change_size, input_files, max_workers=3)
     all_change_sizes = []
     for change_sizes in res:
         all_change_sizes.extend(change_sizes)
@@ -177,9 +199,9 @@ if __name__ == '__main__':
 
     plt.hist(np.array(all_change_sizes), bins=list(range(40)))
     plt.title("Size of changes")
-    plt.ylabel("#Occurances")'''
+    plt.ylabel("#Occurances")"""
 
-    '''res = process_map(count_creation_and_deletion_numbers, input_files, max_workers=3)
+    """res = process_map(count_creation_and_deletion_numbers, input_files, max_workers=3)
     all_creations = 0
     all_deletions = 0
     all_edits = 0
@@ -190,11 +212,11 @@ if __name__ == '__main__':
 
     print(f'Total Edits: {all_edits}')
     print(f'creations: {all_creations} \t\t % {all_creations / all_edits}')
-    print(f'deletions: {all_deletions} \t\t % {all_deletions / all_edits}')'''
+    print(f'deletions: {all_deletions} \t\t % {all_deletions / all_edits}')"""
 
-    '''Total Edits: 282713251
+    """Total Edits: 282713251
     creations: 143045982 		 % 0.5059755122691437
-    deletions: 57379699 		 % 0.20296076960326137'''
+    deletions: 57379699 		 % 0.20296076960326137"""
 
     res = process_map(get_all_valid_times, input_files, max_workers=1)
 
@@ -202,5 +224,5 @@ if __name__ == '__main__':
     for a in res:
         all_times.extend(a)
     del res
-    with open(raw_data_folder.joinpath('all_times.pickle'), 'wb') as file:
+    with open(raw_data_folder.joinpath("all_times.pickle"), "wb") as file:
         pickle.dump(all_times, file)
