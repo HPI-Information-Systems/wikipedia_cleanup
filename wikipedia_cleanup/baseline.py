e9a6c295d4d9525bd5e32d385ba05e5e5502af0b
Niklas Köhnecke <niklas.koehnecke@gmail.com>
2021-11-02 13:17:40 +0100

    Test to see if pre-commit worked

--- wikipedia_cleanup/baseline.py
+++ wikipedia_cleanup/baseline.py
@@ -2,8 +2,8 @@ import numpy as np
 
 
 def next_change(previous_change_timestamps):
-    previous_change_timestamps = np.array(sorted(previous_change_timestamps))
-    if len(previous_change_timestamps) > 2:
-        mean_time_to_change = np.median(previous_change_timestamps[1:] - previous_change_timestamps[0:-1])
-        return previous_change_timestamps[-1] + mean_time_to_change
-    return None
+    previous_change_timestamps = np.sort(np.unique(previous_change_timestamps))
+    if len(previous_change_timestamps) < 2:
+        return None
+    mean_time_to_change = np.median(previous_change_timestamps[1:] - previous_change_timestamps[0:-1])
+    return previous_change_timestamps[-1] + mean_time_to_change
