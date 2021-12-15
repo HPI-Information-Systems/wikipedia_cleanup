56ddc48a2e75703e1900b95387db82e028b5a91b
Martin Buessemeyer <martin.buessemeyer@student.hpi.de>
2021-12-15 19:02:26 +0100

    Finalize link frequency analysis

--- wikipedia_cleanup/property_correlation.py
+++ wikipedia_cleanup/property_correlation.py
@@ -18,12 +18,12 @@ class PropertyCorrelationPredictor(Predictor):
         regex_str = '\\[\\[((?:\\w+:)?[^<>\\[\\]"\\|]+)(?:\\|[^\\n\\]]+)?\\]\\]'
         regex = re.compile(regex_str)
 
-        infobox_key_to_related_page_titles = {}
+        related_page_index = {}
 
-        grouped_infoboxes = train_data.groupby("infobox_key")["current_value"].unique()
+        grouped_infoboxes = train_data.groupby("page_title")["current_value"].unique()
 
         for key, row in grouped_infoboxes.iteritems():
-            infobox_key_to_related_page_titles[key] = list(
+            related_page_index[key] = list(
                 set(
                     match.groups()[0].strip()
                     for value in row
@@ -33,7 +33,7 @@ class PropertyCorrelationPredictor(Predictor):
                 )
             )
 
-        return infobox_key_to_related_page_titles
+        return related_page_index
 
     @staticmethod
     def _create_time_series(a, duration):
@@ -64,7 +64,7 @@ class PropertyCorrelationPredictor(Predictor):
 
     def fit(self, train_data: pd.DataFrame, last_day: datetime) -> None:
 
-        infobox_key_to_related_page_titles = self._get_links(train_data)
+        related_page_index = self._get_links(train_data)
 
         def percentage_manhatten_adaptive_time_lag(arr1, arr2):
             DELAY_RANGE = 3
@@ -100,11 +100,9 @@ class PropertyCorrelationPredictor(Predictor):
             ["property_name", "bin_idx", "infobox_key"]
         ].agg(list)
 
-        infobox_key_to_related_page_titles_filtered = {}
-
         links = set()
 
-        for infobox_key, related_pages in infobox_key_to_related_page_titles.items():
+        for related_pages in related_page_index.values():
             for related_page in related_pages:
                 links.add(related_page)
 
@@ -118,17 +116,17 @@ class PropertyCorrelationPredictor(Predictor):
             ]
         )
 
-        for infobox_key, related_pages in tqdm(
-            infobox_key_to_related_page_titles.items(),
-            total=len(infobox_key_to_related_page_titles),
-        ):
+        filtered_related_page_index = {}
+        for page_title, related_pages in related_page_index.items():
             found_related_pages = []
             for related_page in related_pages:
-                if related_page in links_found:
+                if (
+                    related_page in links_found
+                    and page_title in related_page_index[related_page]
+                    and related_pages != page_title
+                ):
                     found_related_pages.append(related_page)
-            infobox_key_to_related_page_titles_filtered[
-                infobox_key
-            ] = found_related_pages
+            filtered_related_page_index[page_title] = found_related_pages
 
         max_dist = 0.05
 
@@ -138,26 +136,17 @@ class PropertyCorrelationPredictor(Predictor):
             matches = []
             for key, row in tqdm(page_id_groups.iterrows(), total=len(page_id_groups)):
                 if len(row[1]) > 1:
-                    infobox_keys_on_page = set(row[2])
-                    related_page_names = set()
-                    for infobox_key in infobox_keys_on_page:
-                        related_page_names.update(
-                            infobox_key_to_related_page_titles_filtered[infobox_key]
-                        )
-                    related_page_names.discard(key)
                     for related_key, related_row in page_id_groups.loc[
-                        list(related_page_names)
+                        filtered_related_page_index[key]
                     ].iterrows():
                         row[0].extend(related_row[0])
                         row[1].extend(related_row[1])
                         row[2].extend(related_row[2])
-                        pass
                     input_data = vstack(row[1])
                     neigh = NearestNeighbors(
                         radius=max_dist,
                         metric=percentage_manhatten_adaptive_time_lag_symmetric,
                     )
-                    print(input_data.count_nonzero(), key)
                     neigh.fit(input_data)
                     neighbor_indices = neigh.radius_neighbors(return_distance=False)
                     for i, neighbors in enumerate(neighbor_indices):
