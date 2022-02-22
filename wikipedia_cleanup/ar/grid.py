6e1576bfb4f7edd1144528f5c254f21f330439c3
fabianhe <39628987+fabianhe@users.noreply.github.com>
2022-02-22 10:59:02 +0100

    Update grid

--- wikipedia_cleanup/ar/grid.py
+++ wikipedia_cleanup/ar/grid.py
@@ -1,5 +1,9 @@
+import collections
+import functools
 import itertools
 import math
+import operator
+import resource
 from pathlib import Path
 
 from tqdm.auto import tqdm
@@ -7,52 +11,52 @@ from tqdm.auto import tqdm
 from wikipedia_cleanup.ar.template_predictor import AssociationRulesTemplatePredictor
 from wikipedia_cleanup.predict import TrainAndPredictFramework
 
-n_files = math.ceil(585 / 3)
-n_jobs = 4
-input_path = Path("data/new_custom/")
 
-MIN_SUPPORT = [0.01, 0.02, 0.03, 0.04, 0.05]
-MIN_CONFIDENCE = [0.6, 0.7, 0.8, 0.9]
-MIN_TEMPLATE_SUPPORT = [0.0, 0.001]
-VAL_SIZE = [0.3, 0.2, 0.1]
-VAL_PRECISION = [0.6, 0.7, 0.8, 0.9]
-TRANSACTION_FREQ = ["D", "W"]
-
-for vars in tqdm(
-    itertools.product(
-        MIN_SUPPORT,
-        MIN_CONFIDENCE,
-        MIN_TEMPLATE_SUPPORT,
-        VAL_SIZE,
-        VAL_PRECISION,
-        TRANSACTION_FREQ,
-    ),
-    total=len(MIN_SUPPORT)
-    * len(MIN_CONFIDENCE)
-    * len(MIN_TEMPLATE_SUPPORT)
-    * len(VAL_SIZE)
-    * len(VAL_PRECISION)
-    * len(TRANSACTION_FREQ),
-):
-    (
-        min_support,
-        min_confidence,
-        min_template_support,
-        val_size,
-        val_precision,
-        transaction_freq,
-    ) = vars
-    model = AssociationRulesTemplatePredictor(
-        min_support=min_support,
-        min_confidence=min_confidence,
-        min_template_support=min_template_support,
-        val_size=val_size,
-        val_precision=val_precision,
-        transaction_freq=transaction_freq,
+def generate_grid(**items):
+    grid_generator = itertools.starmap(
+        collections.namedtuple("GridEntry", items.keys()),
+        itertools.product(*items.values()),
     )
+    grid_len = functools.reduce(
+        operator.mul, [len(value) for value in items.values()], 1
+    )
+    return grid_generator, grid_len
+
+
+def limit_memory(maxsize: int) -> None:
+    _, hard = resource.getrlimit(resource.RLIMIT_AS)
+    resource.setrlimit(resource.RLIMIT_AS, (maxsize, hard))
+
+
+limit_memory(128_000_000_000)
+n_files = math.ceil(585 / 3)
+n_jobs = 6
+input_path = Path("data/new_custom/")
+# /san2/data/change-exploration/mp2021_wiki_cleanup/new_costum_filtered_format
+
+grid, grid_len = generate_grid(
+    min_support=[0.01, 0.025, 0.05],
+    min_confidence=[0.7, 0.8, 0.9],
+    min_template_support=[0.0, 0.001],
+    val_size=[0.2, 0.1],
+    val_precision=[0.7, 0.8, 0.9],
+    transaction_freq=["W", "D"],
+)
+for grid_entry in tqdm(grid, total=grid_len):
+    model = AssociationRulesTemplatePredictor(
+        min_support=grid_entry.min_support,
+        min_confidence=grid_entry.min_confidence,
+        min_template_support=grid_entry.min_template_support,
+        val_size=grid_entry.val_size,
+        val_precision=grid_entry.val_precision,
+        transaction_freq=grid_entry.transaction_freq,
+    )
+    run_id = "ar_template_{}".format("_".join(str(var) for var in grid_entry))
     framework = TrainAndPredictFramework(
-        model, group_key=["infobox_key", "property_name"]
+        model, group_key=["infobox_key", "property_name"], run_id=run_id
     )
     framework.load_data(input_path, n_files, n_jobs)
     framework.fit_model()
-    print(framework.test_model(predict_subset=1, save_results=False))
+    framework.test_model()
+    framework.generate_plots()
+    del model, framework
