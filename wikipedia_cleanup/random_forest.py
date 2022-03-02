72f91942fb180999e83dcb349b6c2525b7fe7626
malte-b <malte.barth@student.hpi.de>
2022-03-02 16:06:03 +0100

    add class_clustering

--- wikipedia_cleanup/random_forest.py
+++ wikipedia_cleanup/random_forest.py
@@ -12,7 +12,8 @@ from wikipedia_cleanup.predictor import CachedPredictor
 
 
 class RandomForestPredictor(CachedPredictor):
-    def __init__(self, use_cache: bool = True, threshold: float = 0.0, return_probs: bool = False, min_number_changes: int = 0) -> None:
+    def __init__(self, use_cache: bool = True, threshold: float = 0.0, cluster_classes: bool = False, \
+                       return_probs: bool = False, min_number_changes: int = 0) -> None:
         super().__init__(use_cache)
         # contains for a given infobox_property_name (key) the regressor (value)
         self.classifiers: dict = {}
@@ -21,6 +22,7 @@ class RandomForestPredictor(CachedPredictor):
         self.last_preds: dict = {}
         self.threshold: float = threshold
         self.return_probs: bool = return_probs
+        self.cluster_classes: bool = cluster_classes
         self.min_number_changes: int = min_number_changes
 
     def get_relevant_ids(self, identifier: Tuple) -> List[Tuple]:
@@ -121,25 +123,41 @@ class RandomForestPredictor(CachedPredictor):
             classes = clf.classes_
             pred_probs = clf.predict_proba(X_test)[0]
             
-            if self.return_probs:
-                classes_indices = [i for i,c in enumerate(classes) if first_day_to_predict<= (date_of_last_change + timedelta(days=int(c)))< (first_day_to_predict + timedelta(days=timeframe))]
-                sum_of_probabilites = pred_probs[classes_indices].sum()
-                self.last_preds[data_key_item] = (date_of_last_change, pred_probs)
-            else:
+            if not self.return_probs and not self.cluster_classes:
                 if pred_probs.max() >= self.threshold:
                     pred = int(classes[pred_probs.argmax()])
                 else:
                     pred = 9999
-
                 self.last_preds[data_key_item] = (date_of_last_change, pred)
-        else:
-            if self.return_probs:
-                classes = self.classifiers[data_key_item].classes_
-                pred_probs = self.last_preds[data_key_item][1]
+            else:
                 classes_indices = [i for i,c in enumerate(classes) if first_day_to_predict<= (date_of_last_change + timedelta(days=int(c)))< (first_day_to_predict + timedelta(days=timeframe))]
                 sum_of_probabilites = pred_probs[classes_indices].sum()
-            else:
+                self.last_preds[data_key_item] = (date_of_last_change, timeframe, pred_probs)
+                if self.cluster_classes:
+                    if sum_of_probabilites >= self.threshold:
+                        self.last_preds[data_key_item] = (date_of_last_change, timeframe, pred_probs, True)
+                        return True
+                    else:
+                        self.last_preds[data_key_item] = (date_of_last_change, timeframe, pred_probs, False)
+                        return False
+
+        else:
+            if not self.return_probs and not self.cluster_classes:
                 pred = self.last_preds[data_key_item][1]
+            else:
+                if self.last_preds[data_key_item][1] == timeframe:
+                    return self.last_preds[data_key_item][3]                
+                classes = self.classifiers[data_key_item].classes_
+                pred_probs = self.last_preds[data_key_item][2]
+                classes_indices = [i for i,c in enumerate(classes) if first_day_to_predict<= (date_of_last_change + timedelta(days=int(c)))< (first_day_to_predict + timedelta(days=timeframe))]
+                sum_of_probabilites = pred_probs[classes_indices].sum()
+                if self.cluster_classes:
+                    if sum_of_probabilites >= self.threshold:
+                        self.last_preds[data_key_item] = (date_of_last_change, timeframe, pred_probs,True)
+                        return True
+                    else:
+                        self.last_preds[data_key_item] = (date_of_last_change, timeframe, pred_probs,False)
+                        return False
 
         if self.return_probs:
             return sum_of_probabilites
