80365f3d6605d201b469a656249c8ddfc240d077
Niklas Köhnecke <niklas.koehnecke@gmail.com>
2022-03-16 13:56:53 +0100

    Fix a bug where ensembles shared additional data between themselves

--- wikipedia_cleanup/ensemble.py
+++ wikipedia_cleanup/ensemble.py
@@ -1,6 +1,6 @@
 from abc import ABC
 from datetime import date, datetime
-from typing import Callable, List, Tuple
+from typing import Callable, List, Set, Tuple
 
 import numpy as np
 import pandas as pd
@@ -12,6 +12,7 @@ from wikipedia_cleanup.predictor import Predictor
 class BasicEnsemble(Predictor, ABC):
     def __init__(self, predictors: List[Predictor]):
         self._predictors = predictors
+        self._cur_relevant_ids: List[Set[Tuple]] = [set() for p in self._predictors]
 
     def fit(
         self, train_data: pd.DataFrame, last_day: datetime, keys: List[str]
@@ -27,8 +28,9 @@ class BasicEnsemble(Predictor, ABC):
 
     def get_relevant_ids(self, identifier: Tuple) -> List[Tuple]:
         get_relevant_ids = set()
-        for predictor in self._predictors:
-            get_relevant_ids |= set(predictor.get_relevant_ids(identifier))
+        for i, predictor in enumerate(self._predictors):
+            self._cur_relevant_ids[i] = set(predictor.get_relevant_ids(identifier))
+            get_relevant_ids |= self._cur_relevant_ids[i]
         return list(get_relevant_ids)
 
     def _make_individual_predictions(
@@ -39,12 +41,30 @@ class BasicEnsemble(Predictor, ABC):
         first_day_to_predict: date,
         timeframe: int,
     ) -> List[bool]:
-        return [
-            predictor.predict_timeframe(
-                data_key, additional_data, columns, first_day_to_predict, timeframe
+        key_idx = columns.index("key")
+        predictions = []
+        for i, predictor in enumerate(self._predictors):
+            if len(additional_data) > 0:
+                if len(self._cur_relevant_ids[i]) == 0:
+                    cur_additional_data = additional_data[:0]
+                else:
+                    additional_data_filter = [
+                        cur_key in self._cur_relevant_ids[i]
+                        for cur_key in additional_data[:, key_idx]
+                    ]
+                    cur_additional_data = additional_data[additional_data_filter]
+            else:
+                cur_additional_data = additional_data
+            predictions.append(
+                predictor.predict_timeframe(
+                    data_key,
+                    cur_additional_data,
+                    columns,
+                    first_day_to_predict,
+                    timeframe,
+                )
             )
-            for predictor in self._predictors
-        ]
+        return predictions
 
     def predict_timeframe(
         self,
